d208ff55127ee5ff12766bcd9e3ade0aa8fefa8b
valeria-ab <lera.magnum@gmail.com>
2022-05-28 18:28:49 +0300

    header basic styles

---
+++ src/header/nav/Nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Nav.module.css';
+
+const Nav = () => {
+    return (
+        <div className={s.nav}>
+            <a href="">Главная</a>
+            <a href="">Навыки</a>
+            <a href="">Проекты</a>
+            <a href="">Контакты</a>
+            </div>
+    );
+}
+
+export default Nav; 
